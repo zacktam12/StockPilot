b064b431aac9666bb765ff61bfd412990f883b42
Zekarias Tamiru <zekariastamiru12@gmail.com>
2025-06-08 20:33:12 +0300

    Refactor app.js: organize route imports and middleware setup for better structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,13 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const productRoutes = require("./routes/product.routes");
+const userRoutes = require("./routes/user.routes");
+const categoryRoutes = require("./routes/category.routes");
+const saleRoutes = require("./routes/sale.routes");
+const purchaseRoutes = require("./routes/purchase.routes");
+const roleRoutes = require("./routes/role.routes");
+const settingsRoutes = require("./routes/settings.routes");
+const productSaleRoutes = require("./routes/productSale.routes");
 
 const app = express();
 const prisma = new PrismaClient();
@@ -11,7 +18,14 @@ app.get("/", async (req, res) => {
 });
 
 app.use(express.json());
+app.use("/api/purchases", purchaseRoutes);
+app.use("/api/roles", roleRoutes);
+app.use("/api/settings", settingsRoutes);
+app.use("/api/sales", saleRoutes);
+app.use("/api/categories", categoryRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/product-sales", productSaleRoutes);
 
 // Export app for use in server.js
 module.exports = app;
