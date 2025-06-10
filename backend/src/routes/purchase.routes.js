3f84939174de2de31f993f8c6bdc429c8fa75c73
Zekarias Tamiru <zekariastamiru12@gmail.com>
2025-06-10 23:16:02 +0300

    Refactor category, product, and purchase routes to include validation middleware for create and update operations

--- backend/src/routes/purchase.routes.js
+++ backend/src/routes/purchase.routes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const purchaseController = require("../controller/purchase.controller");
+const {
+  validateCreatePurchase,
+  validateUpdatePurchase,
+} = require("../validators/purchase.validator");
 
-router.post("/", purchaseController.createPurchase);
+router.post("/", validateCreatePurchase, purchaseController.createPurchase);
+router.put("/:id", validateUpdatePurchase, purchaseController.updatePurchase);
+// router.post("/", purchaseController.createPurchase);
 router.get("/", purchaseController.getAllPurchases);
 router.get("/:id", purchaseController.getPurchaseById);
 router.put("/:id", purchaseController.updatePurchase);
