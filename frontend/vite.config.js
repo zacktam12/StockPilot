2ea508fb1b5e0915d4ce315c3c971c9300422b35
Zekarias Tamiru <zekariastamiru12@gmail.com>
2025-06-15 20:54:24 +0300

    add neccessary packages

---
+++ frontend/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true, // Add this line
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("sentry")) {
+            return "sentry";
+          }
+        },
+      },
+    },
+  },
+});
