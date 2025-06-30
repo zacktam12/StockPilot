3fc89c2d0ce6fcd6169330d3dd5093b3f3748220
Zekarias Tamiru <zekariastamiru12@gmail.com>
2025-06-30 16:39:33 +0300

    add functionalities to supplier validator

--- backend/src/validators/supplier.validator.js
+++ backend/src/validators/supplier.validator.js
@@ -2,9 +2,11 @@ const Joi = require("joi");
 
 const supplierSchema = Joi.object({
   name: Joi.string().min(2).max(100).required(),
+  contactName: Joi.string().max(100).allow(null, ""),
   email: Joi.string().email().allow(null, ""),
   phone: Joi.string().max(30).allow(null, ""),
   address: Joi.string().max(255).allow(null, ""),
+  companyName: Joi.string().max(100).allow(null, ""),
 });
 
 const validateCreateSupplier = (req, res, next) => {
