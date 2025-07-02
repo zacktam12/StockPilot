108853c1125c47402e38dc2c8c04b7177ecd36b7
Zekarias Tamiru <zekariastamiru12@gmail.com>
2025-07-02 21:04:00 +0300

    feat: Add Customerrepository with CRUD operations for customer management

---
+++ backend/src/repositories/customer.repository.js
@@ -0,0 +1,38 @@
+const BaseRepository = require("../utils/BaseRepository");
+const { prisma } = require("../config/db");
+
+class CustomerRepository extends BaseRepository {
+  constructor() {
+    super(prisma.customer);
+  }
+
+  async findCustomers(
+    page = 1,
+    limit = 10,
+    search = "",
+    sortField = "createdAt",
+    sortOrder = "desc"
+  ) {
+    const where = {
+      ...(search && {
+        OR: [
+          { name: { contains: search } },
+          { email: { contains: search } },
+          { phone: { contains: search } },
+          { address: { contains: search } },
+        ],
+      }),
+    };
+
+    const orderBy = {};
+    if (sortField && sortOrder) {
+      orderBy[sortField] = sortOrder.toLowerCase();
+    } else {
+      orderBy.createdAt = "desc";
+    }
+
+    return this.findManyWithPagination(where, page, limit, {}, orderBy);
+  }
+}
+
+module.exports = new CustomerRepository();
